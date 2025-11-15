f0743e44de7e72698a0b3d5328c46ab96adef27c
Jagadeesh <vedulajagadeesh1994@gmail.com>
2025-11-15 12:21:56 +0530

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install Flutter
+echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:$(pwd)/flutter/bin"
 
@@ -8,5 +9,10 @@ export PATH="$PATH:$(pwd)/flutter/bin"
 flutter --version
 
 # Get dependencies and build
+echo "Getting dependencies..."
 flutter pub get
+
+echo "Building web..."
 flutter build web
+
+echo "Build completed!"
